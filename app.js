98f05a7ffd227295d4bb4556597d5931463c90b7
qReleasep <autsada09381@gmail.com>
2016-11-13 21:47:06 +0700

    fix line

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-angular.module('ContactApp' , [])
-  .service('contactService',function(){
+angular.module( 'Todos',[])
+  .service('Services',function(){
     var self = this
 
-    self.contacts = [
-      { fullname: 'Narez Jaroenphum', nickname: 'Ice', phone: '087-xxx-xxxx'},
-      { fullname: 'Somchai Tungwinyoo', nickname: 'Zong', phone: '082-xxx-xxxx'},
-      { fullname: 'Veerasak Supakrit', nickname: 'Good', phone: '083-xxx-xxxx'}
-    ]
+    self.Todo = []
 
-    self.list = function() {
-      return self.contacts
-    }
-    self.add = function (contact){
-      self.contacts.push(contact)
-    }
-  })
-  .controller('ListContactController', function ($scope,contactService) {
-    $scope.contacts = contactService.list();
-  })
-  .controller('AddContactController' , function($scope,contactService){
+    self.list = function() {return self.Todo}
+    self.add = function(Todo) {self.Todo.push(Todo)}
+})
+
+  .controller('ListMessageController',function ($scope, Services){
+    $scope.Todo = Services.list()
+})
+
+  .controller('AddMessageController', function($scope, Services){
+    $scope.title = ''
     $scope.save = function(){
-      var contact = {
-        fullname: $scope.fullname,
-        nickname: $scope.nickname,
-        phone: $scope.phone
+      if($scope.title !== '')
+      {
+        var t = {title: $scope.title}
+        var c = {done: $scope.done}
+        Services.add(t)
+        Services.add(c)
+        resetForm()
       }
 
-      contactService.add(contact)
-      resetForm();
     }
-    function resetForm(){
-      $scope.fullname = ''
-      $scope.nickname = ''
-      $scope.phone = ''
-    }
-  });
+
+    function resetForm(){$scope.title =''}
+})
