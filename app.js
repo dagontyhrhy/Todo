f4b62afa56ceb27d6791c7004ad927febf2201cf
dagontyhrhy <takecom05@gmail.com>
2016-11-17 19:36:44 +0700

    finish

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ angular.module( 'Todoapp',[])
     var self = this
 
 
-    self.Todo = []
-
+    self.Todo = [{title:'Autsada',done:true},{title:'Tosapol',done:false}];
     self.list = function() {return self.Todo}
-    self.add = function(Todo) {self.Todo.push(Todo)}
-})
+    self.add = function(t) {self.Todo.push(t)}
 
+self.Check = function(ti) {
+            if (ti.done=== false) {
+                ti.done = true
+            } else if (ti.done === true) {
+                ti.done = false
+            }
+        }
+})
   .controller('ListMessageController',function ($scope, Services){
     $scope.Todo = Services.list()
+    $scope.Check = function(ti) {
+            Services.Check(ti)
+        }
 })
 
   .controller('AddMessageController', function($scope, Services){
-    $scope.title = ''
-    $scope.save = function(){
-      if($scope.title !== '')
-      {
-        var t = {title: $scope.title}
-        var c = {done: $scope.done}
-        Services.add(t)
-        resetForm()
-      }
-    }
+   $scope.title = '  '
+        $scope.add = function() {
+            if ($scope.title !== '  ') {
+                var t = {title: $scope.title,done: false}
+                Services.add(t);
+                resetForm();
+            }
+        }
 
-    function resetForm(){$scope.title =''}
+    function resetForm(){$scope.title =' '}
 })
